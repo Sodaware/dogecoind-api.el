149f53eb89dc9eb2f0c401357e1622269c1d6aaa
Phil Newton <phil@sodaware.net>
2014-10-02 20:40:40 -0400

    Added initial helper functions with basic tests

---
+++ dogecoind-api.el
@@ -0,0 +1,98 @@
+;;; dogecoind-api.el --- API library for working with dogecoind
+
+;; Copyright (C) 2014 Phil Newton
+
+;; Author: Phil Newton <phil@sodaware.net>
+;; Version: 1.0.0
+;; Package-Requires: ((json "1.2"))
+;; Keywords: dogecoin
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; dogecoind-api provides library functionality for working with the dogecoind
+;; process.
+
+;;; Code:
+
+;; Dependencies
+
+(require 'json)
+(require 'url-http)
+
+(defvar url-http-end-of-headers)
+
+;; Configuration
+
+(defgroup dogecoind-api nil
+  "dogecoind api extension"
+  :group 'processes
+  :prefix "dogecoind-api-")
+
+(defcustom dogecoind-api-username nil
+  "Your dogecoind username."
+  :group 'dogecoind-api
+  :type '(string))
+
+(defcustom dogecoind-api-password nil
+  "Your dogecoind password."
+  :group 'dogecoind-api
+  :type '(string))
+
+(defcustom dogecoind-api-poit nil
+  "Your dogecoind server port."
+  :group 'dogecoind-api
+  :type '(integer))
+
+
+;; Main API methods
+
+(defun dogecoind-api-get-info ()
+  "Get an object containing various state info."
+  
+
+  (message (format "%s" (dogecoind-api--get-request "getinfo")))
+  
+  ;; Connect
+  
+  )
+
+
+;; Request helpers
+
+(defun dogecoind-api--get-request (action &optional params)
+  "Request the server to perform ACTION with optional PARAMS."
+  (let* ((http-auth-token (dogecoind-api--create-auth dogecoind-api-username dogecoind-api-password))
+         (url-request-method "GET")
+         (url-request-extra-headers
+          `(("Authorization" . ,(concat "Basic " http-auth-token))))
+         (url-request-data "{ \"method\": \"getinfo\" }"))
+
+    (with-current-buffer (url-retrieve-synchronously "http://127.0.0.1:8334/")
+      (goto-char (point-min))
+      (goto-char url-http-end-of-headers)
+      (prog1 (json-read)
+        (kill-buffer)))))
+
+(defun dogecoind-api--build-request (action &optional params)
+  "Use ACTION, and optionally PARAMS, to build the JSON payload for an RPC request."
+  (json-encode `((:action . ,action))))
+
+(defun dogecoind-api--create-auth (username password)
+  "Use USERNAME and PASSWORD to create a HTTP authorization token."
+  (base64-encode-string (concat username ":" password) t))
+
+(provide 'dogecoind-api)
+;;; dogecoind-api.el ends here
---
+++ test/dogecoind-api-test.el
@@ -0,0 +1,12 @@
+
+;; INTERNAL TESTS
+
+(ert-deftest dogecoind-api-test/can-encode-auth ()
+  (should (string=
+           "dXNlcm5hbWU6cGFzc3dvcmQ="
+           (dogecoind-api--create-auth "username" "password"))))
+
+(ert-deftest dogecoind-api-test/can-build-request-without-params ()
+  (should (string=
+           "{\"action\":\"getinfo\"}"
+           (dogecoind-api--build-request "getinfo"))))
