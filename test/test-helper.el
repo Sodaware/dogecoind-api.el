59bff2ca9a5215b3c0b7b6888ad1af97f78080e3
Phil Newton <phil@sodaware.net>
2014-10-03 09:05:45 -0400

    Added el-mock requirement to Cask file

--- test/test-helper.el
+++ test/test-helper.el
@@ -1,9 +1,18 @@
+;; Required testing libraries
+(require 'cl)
+(require 'el-mock)
+
+;; Load the library 
 (add-to-list 'load-path (file-name-directory (directory-file-name (file-name-directory load-file-name))))
 (require 'dogecoind-api)
 
+;; Setup test paths
 (setq test-directory (file-name-directory (directory-file-name (file-name-directory load-file-name))))
 
+;; Helper functions
+
 (defun read-fixture (file)
+  "Reads FILE from the fixtures directory and returns it as parsed JSON."
   
   ;; Get the file path
   (let* ((file-path (concat test-directory "/fixtures/" file))
